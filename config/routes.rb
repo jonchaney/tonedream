aa9b882a2b5d75d3169f388d51a575079f183948
Jonathan Chaney <jonchaney@gmail.com>
2017-11-17 14:13:57 -0800

    add artist routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,11 +9,11 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     resources :users, only: [:create, :update, :show]
     resource :session, only: [:create, :destroy, :show]
+    resources :artists, only: [:create, :destroy, :show, :update, :index]
     resources :albums, only: [:create, :destroy, :show, :update, :index]
     resources :tracks, only: [:create, :destroy, :show, :update, :index]  
     resources :searches, only: [:index, :show] 
     get '/user/:user_id/albums', to: 'albums#select_albums'
   end
   root "static_pages#root"
-
 end
