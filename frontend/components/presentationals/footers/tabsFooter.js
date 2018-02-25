9fc66da885272dd10fd01e14e9edec1e1ca49c93
Jonathan Chaney <jonchaney@gmail.com>
2018-02-24 16:51:19 -0800

    added npm fontawesome and it works

--- frontend/components/presentationals/footers/tabsFooter.js
+++ frontend/components/presentationals/footers/tabsFooter.js
@@ -1,14 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Tab from '../tab.js';
-import fontawesome from '@fortawesome/fontawesome'
-import faHome from '@fortawesome/fontawesome-pro-light/faHome'
-import faSearch from '@fortawesome/fontawesome-pro-light/faSearch'
-import faBars from '@fortawesome/fontawesome-pro-light/faBars'
-
-fontawesome.library.add(faHome)
-fontawesome.library.add(faSearch)
-fontawesome.library.add(faBars)
 
 class TabsFooter extends React.Component {
   constructor(props) {
@@ -19,8 +11,9 @@ class TabsFooter extends React.Component {
     return (
         <footer>
           <Tab icon="fal fa-home"/>
+          <Tab icon="fal fa-calendar"/>
           <Tab icon="fal fa-search"/>
-          {/* <Tab icon="far fa-envelope"/> */}
+          <Tab icon="fal fa-paper-plane"/>
           <Tab icon="fal fa-bars"/>
         </footer>
     );
--- frontend/components/presentationals/tab.js
+++ frontend/components/presentationals/tab.js
@@ -1,6 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import fontawesome from '@fortawesome/fontawesome'
+import faHome from '@fortawesome/fontawesome-pro-light/faHome'
+import faSearch from '@fortawesome/fontawesome-pro-light/faSearch'
+import faBars from '@fortawesome/fontawesome-pro-light/faBars'
+import faPaperPlane from '@fortawesome/fontawesome-pro-light/faPaperPlane'
+import faCalendar from '@fortawesome/fontawesome-pro-light/faCalendar'
+
+fontawesome.library.add(faHome)
+fontawesome.library.add(faSearch)
+fontawesome.library.add(faBars)
+fontawesome.library.add(faPaperPlane)
+fontawesome.library.add(faCalendar)
+
 class Tab extends React.Component {
   constructor(props) {
     super(props);
