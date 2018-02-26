dfa71028543d025a470e056ef8daca8369b63a82
Jonathan Chaney <jonchaney@gmail.com>
2018-02-25 22:55:37 -0800

    yml

--- config/application.rb
+++ config/application.rb
@@ -21,8 +21,9 @@ module Tonedream
         :access_key_id => ENV["s3_access_key_id"],
         :secret_access_key => ENV["s3_secret_access_key"],
         :s3_region => ENV["s3_region"],
-        s3_host_name: "s3-#{ENV["s3_region"]}.amazonaws.com"
+        s3_host_name: "s3.amazonaws.com"
   }
+
 
 }
     # Settings in config/environments/* take precedence over those specified here.
